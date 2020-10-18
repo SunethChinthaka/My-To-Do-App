fcbe06524ae13ca3bba67c234f2cd43043cc6d47
SunethChinthaka <sunethedu@gmail.com>
2020-10-18 13:23:07 +0530

    DB Created

---
+++ app/src/main/java/com/example/todoapp/DbHandler.java
@@ -0,0 +1,40 @@
+package com.example.todoapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DbHandler extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "todo.db";
+    private static final String TABLE_NAME = "todo";
+    private static final String ID = "id";
+    private static final String TITLE = "title";
+    private static final String DESCRIPTION = "description";
+    private static final String STARTED = "started";
+    private static final String FINISHED = "finished";
+
+    public DbHandler(@Nullable Context context) {
+        super(context, DB_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        sqLiteDatabase.execSQL(" CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT , TITLE TEXT , " +
+                                "DESCRIPTION TEXT , STARTED TEXT , FINISHED TEXT  ) ");
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+
+
+    }
+}
